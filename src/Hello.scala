0118baa304be57c899179c47e2c4206d5c18a35b
Farhan <farhan-HP-ProBook-4430s>
2013-09-09 00:40:32 +0600

    initila

---
+++ src/Hello.scala
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: farhan
+ * Date: 9/9/13
+ * Time: 12:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+object Hello
+{
+  def main(args: Array[String])
+  {
+    println("Hello Scala")
+  }
+
+}
